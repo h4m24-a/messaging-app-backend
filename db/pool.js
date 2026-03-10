384b9fb65f5625bcdc6c4bd5ad81cbfb4b7938aa
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-03-10 03:31:22 +0000

    set up pool of database connections

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,12 @@
-const { pool, Pool } = require("pg");
+const {  Pool } = require("pg");
 require("dotenv").config();  //load environment variables from a .env file into process.env
 
+// A connection pool is a collection of reusable database connections that can be used by the application to execute queries.
+
 module.export = new Pool({
-  
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
 })
