ff066fc92cd98eba93ace87d97c13ca3b2d85218
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-04-30 22:35:09 +0100

    set up routes for conversation

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get('/', req, res => {
-  res.send('Hello World')
-})
 
+// GET- View all conversations for authenticated user
+router.get('/conversations', )
+
+
+// GET- a single conversation (metadata)
+router.get('/conversations/:conversationId')
+
+
+// GET- View one conversation and messages  
+router.get('/conversations/:conversationId/messages')
+
+
+// POST- Send new message
+router.post('/conversations/:conversationId/messages')
+
+
+// PUT- Update message
+router.put('/conversations/:conversationId/messages/:messageId');
+
+
+// DELETE- Delete message
+router.delete('/conversations/:conversationId/messages/:messageId');
+
+
+// Mark messages as read
+router.patch('/conversations/:conversationId/read')
 
 
 module.exports = router;
