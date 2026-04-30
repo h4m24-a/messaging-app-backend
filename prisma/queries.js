84c5b50b92d0404a0f9288021b0239f8bab4135e
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-04-30 22:35:48 +0100

    set up queries for jwt and user profile

--- prisma/queries.js
+++ prisma/queries.js
@@ -1,7 +1,246 @@
-const { PrismaClient } = require('@prisma/client')   // This imports the PrismaClient class from the @prisma/client package.
+import { PrismaClient } from '@prisma/client';  // This imports the PrismaClient class from the @prisma/client package.
 
 const prisma = new PrismaClient();    // creates an instance of PrismaClient, The prisma object acts as your connection to the database and provides methods to interact with different tables
 
+
+
+//! Conversation & Messages
+// View all converations and one message -  for home page
+async function getAllConversations(userId) {
+  try {
+    const conversations =  await prisma.conversation.findMany( {
+      where: {
+        OR : [                                    // Get converstations even if one user hasn't sent a message
+          { user1Id : userId},
+          { user2Id : userId}
+        ]
+      },
+      include: {
+        messages: {
+          orderBy: {
+            created_at: 'desc',
+          },
+          take: 1,                     // displays the latest message
+          select: {
+            text: true,
+            sender: true,
+            created_at: true,
+            seen: true                // boolean value depending on value
+          },
+        }
+      }
+    })
+
+    return conversations;
+  } catch (error) {
+    console.error('Error fetching conversations:', error);
+    throw error;
+  } 
+}
+
+
+
+
+// View a single conversation 
+async function getSingleConversation(conversationId) {
+  try {
+    const conversation = await prisma.conversation.findUnique({
+      where: {
+        id: conversationId
+      },
+      include: {
+        messages: {
+          orderBy:{
+            created_at: 'desc'
+          },
+          include: {
+            sender: true
+          }
+        },
+        user1: true,
+        user2: true,
+      }
+    })
+    return conversation;
+  } catch (error) {
+    console.error('Error fetching conversation:', error);
+    throw error;
+    
+  }
+}
+
+
+
+
+async function getMessagesInConversation(conversationId, userId) {
+  try {
+    const messages = await prisma.messages.findMany({
+      where: {
+        conversationId: conversationId,
+        conversation: {                   // conversation - enforce that the requesting user belongs to that conversation
+          OR: [
+            { user1Id: userId },
+            { user2Id: userId }
+          ]
+        }
+      },
+      orderBy: {
+        created_at: "asc"
+      },
+      include: {
+        sender: true
+      }
+    });
+
+    return messages;
+  } catch (error) {
+    console.error("Error fetching messages", error);
+    throw error;
+  }
+}
+
+
+
+// Get A Single message
+async function getSingleMessage(messageId, userId) {
+  try {
+    const message = await prisma.messages.findFirst({
+      where: {
+        id: messageId,
+        senderId: userId
+      }
+    })
+    
+  } catch (error) {
+    console.error('Error fetching message', error);
+    throw error
+  }
+  
+}
+
+
+
+
+// Send a message
+async function createMessage(text, conversationId, userId) {
+  try {
+    const newMessage = await prisma.messages.create({
+      data: {
+        text: text,
+        senderId: userId,
+        conversationId: conversationId
+
+      }
+    });
+    return newMessage;
+    
+  } catch (error) {
+    console.error('Error sending message', error);
+    throw error
+  }
+  
+}
+
+
+
+// Update a message
+async function updateMessage(updatedText, userId, messageId) {
+  try {
+    const updatedMessage =  await prisma.messages.update({
+      where: {
+        id: messageId,
+        senderId: userId
+      },
+      data: {
+        text: updatedText
+      }
+    })
+  } catch (error) {
+    console.error('Error updating message', error);
+    throw error
+  }
+
+}
+
+
+
+// Delete a message
+async function deleteMessage(messageId, userId) {
+  try {
+    await prisma.messages.delete({
+      where: {
+        id: messageId,
+        senderId: userId
+      }
+    })
+  } catch (error) {
+    console.error('Error deleting message', error);
+    throw error
+  }
+}
+
+
+
+
+//! Profile
+
+
+// Get user profile - username, profile image, bio
+async function getProfileOfUser(userId) {
+  try {
+    const profile = await prisma.users.findUnique({
+      where: {
+        id: userId
+      },
+      select: {
+        username: true,
+        profile_image: true,
+        bio: true
+      }
+    })
+  } catch (error) {
+    console.error('Error fetching user profile', error);
+    throw error
+  }
+}
+
+
+// create profile - add profile image & bio
+async function createProfile(userId, profileImage, bio) {
+  try {
+    const profile = await prisma.users.create({
+      data: {
+        id: userId,
+        profile_image: profileImage,      // use .optional for both input forms
+        bio: bio
+      }
+    })
+    
+  } catch (error) {
+    console.error('Error creating user profile', error);
+    throw error
+  } 
+}
+
+
+// Update profile - add profile image & bio
+async function updateProfile(userId, updatedProfileImage, updatedBio) {
+  try {
+    const profile = await prisma.users.update({
+      where: {
+        id: userId
+      },
+      data: {
+        profile_image: updatedProfileImage,      // use .optional for both input forms
+        bio: updatedBio
+      }
+    })
+    return profile
+  } catch (error) {
+    console.error('Error updating user profile', error);
+    throw error
+  } 
+}
 
 
 
@@ -126,6 +365,16 @@ async function deleteRefreshToken(userId) {
 
 
 module.exports = {
+  getAllConversations,
+  getSingleConversation,
+  getMessagesInConversation,
+  getSingleMessage,
+  createMessage,
+  updateMessage,
+  deleteMessage,
+  getProfileOfUser,
+  updateProfile,
+
   insertUser,
   findUserByUsername,
   selectUserById,
