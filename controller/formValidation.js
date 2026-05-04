53c0a70e75abbe7e45b24e720426314349c978ee
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-05-04 19:31:06 +0100

    form validation and sanitization for message input

--- controller/formValidation.js
+++ controller/formValidation.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');  // form validation
 
 const validateProfile= [
   body('bio')
-  .optional({ checkFalsy: true })
+  .optional({ checkFalsy: true })   // bio is not required
     .trim()
     .isLength({ min: 3, max: 100 })
     .withMessage('Bio must be between 3 and 100 characters long.')
@@ -10,10 +10,62 @@ const validateProfile= [
     
     
     body('profileImage')
-    .optional({ checkFalsy: true })
+    .optional({ checkFalsy: true })   // values that are considered falsy in JavaScript are treated as if the field is not provided.
 ];
 
 
+const validateUpdatedProfile= [
+  body('updatedBio')
+  .optional({ checkFalsy: true })   // bio is not required
+    .trim()
+    .isLength({ min: 3, max: 100 })
+    .withMessage('Bio must be between 3 and 100 characters long.')
+    .escape(), // Sanitization: Escape special characters,
+    
+    
+    body('updatedProfileImage')
+    .optional({ checkFalsy: true })   // values that are considered falsy in JavaScript are treated as if the field is not provided.
+];
 
 
-module.exports = { validateProfile  };
+const validateMessage = [
+  body('message')
+  
+  .exists({ checkFalsy: true })
+  .withMessage('Message is required')
+  .trim()
+  .isLength({ min: 3, max: 100 })
+  .withMessage('Message must be between 3 and 100 characters')
+  .isString().withMessage('Message must be a string')
+  .customSanitizer(value => String(value))
+  .escape() // prevents HTML/script injection
+]
+
+
+const validateUpdatedMessage = [
+  body('updatedMessage')
+  
+  .exists({ checkFalsy: true })
+  .withMessage('Message is required')
+  .trim()
+  .isLength({ min: 3, max: 100 })
+  .withMessage('Message must be between 3 and 100 characters')
+  .isString().withMessage('Message must be a string')
+  .customSanitizer(value => String(value))
+  .escape(), // prevents HTML/script injection
+]
+
+module.exports = { validateProfile, validateUpdatedProfile, validateMessage, validateUpdatedMessage  };
+
+
+
+/*
+falsy values include:
+
+"" (empty string)
+0
+false
+null
+undefined
+NaN
+*/
