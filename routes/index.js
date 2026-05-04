00dfa1829735724ba91d60eae3edbe83fd98ef8d
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-05-04 19:31:56 +0100

    mapping controller functions to corresponding routes

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const { viewAllConversations, getSingleConversation, getMessagesInConversation, sendMessage, updateMessage, deleteMessage, markMessageAsSeen } = require('../controller/conversationController');
+const { validateMessage, validateUpdatedMessage } = require('../controller/formValidation')
 
 
 // GET- View all conversations for authenticated user
-router.get('/conversations', )
+router.get('/conversations', viewAllConversations )
 
 
 // GET- a single conversation (metadata)
-router.get('/conversations/:conversationId')
+router.get('/conversations/:conversationId', getSingleConversation)
 
 
 // GET- View one conversation and messages  
-router.get('/conversations/:conversationId/messages')
+router.get('/conversations/:conversationId/messages', getMessagesInConversation)
 
 
 // POST- Send new message
-router.post('/conversations/:conversationId/messages')
+router.post('/conversations/:conversationId/messages', validateMessage, sendMessage)
 
 
 // PUT- Update message
-router.put('/conversations/:conversationId/messages/:messageId');
+router.put('/conversations/:conversationId/messages/:messageId', validateUpdatedMessage, updateMessage);
 
 
 // DELETE- Delete message
-router.delete('/conversations/:conversationId/messages/:messageId');
+router.delete('/conversations/:conversationId/messages/:messageId', deleteMessage);
 
 
 // Mark messages as seen
-router.patch('/conversations/:conversationId/messages/seen')
+router.patch('/conversations/:conversationId/messages/seen', markMessageAsSeen)
 
 
 module.exports = router;
