5b6c75aa62bc1585d6e0e1ec28ea8d73a9796b40
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-07-04 15:44:17 +0100

    hosting

--- app.js
+++ app.js
@@ -13,24 +13,6 @@ const jwtAuthentication = require('./middleware/jwtAuthentication')
 
 const app = express();
 
-
-// Initialize Passport
-app.use(passport.initialize());
-
-// Routes
-const indexRouter = require("./routes/index")
-const authRouter = require("./auth/authRoutes")
-const userRouter = require('./routes/userRouter')
-
-// Serve static files
-app.use(express.static('public'))   // 'public' is my static folder.
-
-
-// Middlewares for cookies
-app.use(cookieParser());
-
-
-
 app.set("trust proxy", 1);
 
 const corsOptions = {
@@ -42,6 +24,19 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// Initialize Passport
+app.use(passport.initialize());
+
+// Serve static files
+app.use(express.static('public'))   // 'public' is my static folder.
+
+
+// Middlewares for cookies
+app.use(cookieParser());
+
+
+
+
 // Body Parser Middleware
 app.use(express.json()); // submit raw json
 app.use(express.urlencoded({ extended: true }));  // This middleware parses this data and makes it available in req.body as a js object. express.urlencoded() is a built-in middleware function in Express that parses incoming requests with URL-encoded payloads. Used when data is submitted from HTML
@@ -49,6 +44,11 @@ app.use(express.urlencoded({ extended: true }));  // This middleware parses this
 
 // Session set up
 
+
+// Routes
+const indexRouter = require("./routes/index")
+const authRouter = require("./auth/authRoutes")
+const userRouter = require('./routes/userRouter')
 
 
 // Adding route-handling code to the request handling chain. This will define particular routes for the different parts of the site
