82991e334a82dd6997af6e30dc07817f21ed2d4a
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-07-04 15:50:29 +0100

    hosting

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // HARD FIX for preflight
-app.options("/*", cors(corsOptions));
+app.options("*", cors());
 
 // Initialize Passport
 app.use(passport.initialize());
